0aec52b146fbcf00a75ddaaf07655ce6784f4cd0
JacksonTheMaster <jakob.langisch@gmail.com>
2025-10-19 03:59:41 +0200

    added GetSingleArgFromRunfile for rf arg (flag) retrieval

--- types.go
+++ types.go
@@ -9,3 +9,13 @@ type SettingsResponse struct {
 	Message string `json:"message"`
 	Status  string `json:"status"`
 }
+
+type ArgsRequest struct {
+	Flag string `json:"flag"`
+}
+
+type ArgResponse struct {
+	Value  string `json:"value,omitempty"`
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
---
+++ utils.go
@@ -0,0 +1,33 @@
+package PluginLib
+
+import (
+	"fmt"
+)
+
+var serverStatusResponse ServerStatusResponse
+var getArgResponse ArgResponse
+
+func GetServerStatus() (ServerStatusResponse, error) {
+	if _, err := Get("/api/v2/server/status", &serverStatusResponse); err != nil {
+		fmt.Println("Error:", err)
+		return serverStatusResponse, err
+	}
+	return serverStatusResponse, nil
+}
+
+func GetSingleArgFromRunfile(flag string) (string, error) {
+	// Create LogRequest struct internally
+	payload := ArgsRequest{
+		Flag: flag,
+	}
+
+	_, err := Post("/api/v2/runfile/args/getarg", payload, &getArgResponse)
+	if err != nil {
+		return "", fmt.Errorf("failed to get arg from runfile api: %v", err)
+	}
+	if getArgResponse.Status != "success" {
+		return "", fmt.Errorf("failed to log a line: %s", getArgResponse.Error)
+	}
+	return getArgResponse.Value, nil
+
+}
